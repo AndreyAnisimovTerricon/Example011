8446c9736fca94006c759bb10bbecf09166184cd
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-29 21:10:47 +0300

    программа определения палиндрома

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿   Console.Write("Enter a number to check if palindrome: ");
+    int a = int.Parse(Console.ReadLine());
+
+        string str = a.ToString();
+        if(str[0]==str[4] && str[1]==str[3])
+        {
+            Console.WriteLine( "Палиндром");
+        }
+    else
+    {
+        Console.WriteLine( "Не палиндром"); 
+    }
